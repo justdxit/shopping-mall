71d677c150a57df582378c709453c3fb4d60dcc6
justdxit <flashback719@gmail.com>
2025-01-22 14:31:20 +0900

    Initial commit without ignored files

---
+++ backend/src/main/java/com/moodone/shopping_mall/dto/TestReq.java
@@ -0,0 +1,13 @@
+package com.moodone.shopping_mall.dto;
+
+public class TestReq {
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
